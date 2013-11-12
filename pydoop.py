1ba793796ffaa8b94e9c9cc8552d90fd47682edf
Yaolong Huang <airekans@gmail.com>
2013-11-12 23:35:59 +0800

    Change to import the function in the parent

--- pydoop.py
+++ pydoop.py
@@ -10,29 +10,25 @@ from optparse import OptionParser
 import importlib
 
 
-def child_main(job_file, func_name, rpipe):
-    if not job_file:
-        print 'job file cannot be empty'
-        return 1
-    
-    mod_name = os.path.basename(job_file).split('.')
+def import_func(mod_file, func_name):
+    mod_name = os.path.basename(mod_file).split('.')
     mod_name = mod_name[0]
 
     try:
         mod = importlib.import_module(mod_name)
     except ImportError:
         print 'cannot import module', mod_name
-        return 1
+        return None
     
     try:
         entry_func = mod.__dict__.get(func_name)
     except TypeError:
-        return 1
+        return None
     
-    if not entry_func:
-        print 'cannot find function %s in module %s' % (func_name, mod_name)
-        return 1
-    
+    return entry_func
+
+
+def child_main(entry_func, rpipe):
     for line in rpipe:
         entry_func(line)
 
@@ -72,6 +68,12 @@ def main(argv=None):
     in_file = args[1]
     
     # MAIN BODY #
+    # first try to import the specified function
+    if opts.func:
+        child_entry_func = import_func(job_file, opts.func)
+        if child_entry_func is None:
+            parser.error('Cannot import function' + opts.func)
+    
     child_pids = []
     for _i in xrange(worker_num):
         if opts.func:
@@ -86,7 +88,7 @@ def main(argv=None):
                 os.close(wfd)
                 rpipe = os.fdopen(rfd, 'r')
                 try:
-                    sys.exit(child_main(job_file, opts.func, rpipe))
+                    sys.exit(child_main(child_entry_func, rpipe))
                 except:
                     sys.exit(1)
             else:
@@ -97,11 +99,14 @@ def main(argv=None):
             pass
     
     with open(in_file) as infd:
-        child_i = 0
-        for line in infd:
-            _, wpipe = child_pids[child_i]
-            wpipe.write(line)
-            child_i = (child_i + 1) % len(child_pids)
+        child_doings = {}
+        child_num = len(child_pids)
+        for i, line in enumerate(infd):
+            pid, wpipe = child_pids[i % child_num]
+            wpipe.write(str(i) + ' ' + line)
+            if pid not in child_doings:
+                child_doings[pid] = []
+            child_doings[pid].append(i)
         
         for _, wpipe in child_pids:
             wpipe.close()
